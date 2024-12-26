1c415da07392e7227f88931d4782aa01a6e9433f
minjae0402 <kangjack0402@gmail.com>
2024-12-26 10:22:06 +0900

    Add : T list

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
-    printf("Hello World\n");
-    printf("src");
+    int arr_size;
+    std::cout << "arr_size : ";
+    std::cin >> arr_size;
+
+    int *list = new int[arr_size];
+    // T *list = new T[arr_size]; T 를 임의의 타임으로 지정
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        std::cin >> list[i];
+    }
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        std::cout << i << "th element of list : " << list[i] << std::endl;
+    }
+    delete[] list;
     return 0;
 }
